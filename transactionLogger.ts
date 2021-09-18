3fb63a39f3e2f6bd607c914470350e6bc0ef9431
Erik <erik.rakuscek@gmail.com>
2021-09-18 15:00:45 +0200

    created wallet dict, filled transaction dict

--- transactionLogger.ts
+++ transactionLogger.ts
@@ -16,30 +16,32 @@ var connectionWs = new web3.Connection(
     connectionWs.onLogs('all', async (logs, ctx) => {
         // for transactions without memo and a lot of text
         if (logs.logs.join(' ').includes('NFT sold')){
+            console.log(logs.signature);
+
             const token: any = {};
             const collection: any = {};
             const block: any = {};
             const transaction: any = {};
+            const wallet: any = {};
             await connectionHttp.getTransaction(logs.signature).then(async info => {
-                console.log(info);
-                console.log("postTokenbalance ",info?.meta?.postTokenBalances);
-                console.log("transMessage ",info?.transaction.message);
-                console.log("transInstraction ",info?.transaction.message.instructions);
                 if (info && info.meta) {
                     token.token_id = info.meta.postTokenBalances ? info.meta.postTokenBalances[0].mint : null;
 
                     const preBalances = info.meta.preBalances;
                     const postBalances = info.meta.postBalances;
-                    token.value = Math.max(...preBalances.map((item, index) => item - postBalances[index]))/1000000000;
+                    const subArray = preBalances.map((item, index) => postBalances[index] - item);
+                    transaction.value = Math.abs(Math.min(...subArray)/1000000000);
+                    transaction.signature = logs.signature;
+                    transaction.recent_blockhash = info.transaction.message.recentBlockhash;
+                    transaction.fee = info.meta.fee;
+                    transaction.vol = 1;
                     
-                    var slotNumber = info?.slot
-                    if (slotNumber != null){
-                        connectionHttp.getBlock(slotNumber).then(async result => { 
-                            block.hash = result?.blockhash;
-                            block.blockTime = result?.blockTime;
-                            console.log("block time:", result?.blockTime, "; block hash:", result?.blockhash)
-                        })}
-
+                    var slotNumber = info?.slot;
+                    if (slotNumber) {
+                        const result = await connectionHttp.getBlock(slotNumber);
+                        block.hash = result?.blockhash;
+                        block.blockTime = result?.blockTime;
+                    }
 
                     const metadata = await getMetadata(token.token_id!)
                     token.name = metadata.data.name;
@@ -57,12 +59,18 @@ var connectionWs = new web3.Connection(
                     }
                     collection.description = token.asset_metadata.collection?.family;
                     collection.external_url = token.asset_metadata.external_url;
+
+                    const idxSeller = subArray.indexOf(Math.max(...subArray));
+                    const idxBuyer = subArray.indexOf(Math.min(...subArray));
+                    wallet.from_wallet_address = info.transaction.message.accountKeys[idxSeller].toString();
+                    wallet.to_wallet_address = info.transaction.message.accountKeys[idxBuyer].toString();
                 }
             }).catch(e => console.log(e));
             console.log(token);
             console.log(collection);
             console.log(block);
-            
+            console.log(transaction);
+            console.log(wallet);
         }
     })
 })();
