ca2223cd8fb33ca8d5b1ec9dc2d6c41d0f7e69c6
Erik <erik.rakuscek@gmail.com>
2021-09-19 18:25:34 +0200

    Fixed 'undefined column' error

--- transactionLogger.ts
+++ transactionLogger.ts
@@ -44,8 +44,8 @@ createDatabase();
                     var slotNumber = info?.slot;
                     if (slotNumber) {
                         const result = await connectionHttp.getBlock(slotNumber);
-                        block.hash = result ? result.blockhash : '';
-                        block.blockTime = result ? result.blockTime : -1;
+                        block.hash = result?.blockhash;
+                        block.block_time = result?.blockTime;
                     }
 
                     const metadata = await getMetadata(token.address!)
@@ -71,34 +71,38 @@ createDatabase();
                     wallet.from_wallet_address = info.transaction.message.accountKeys[idxSeller].toString();
                     wallet.to_wallet_address = info.transaction.message.accountKeys[idxBuyer].toString();
                 }
-            }).catch(e => console.log(e));
 
-            const from_wallet_id = await insert(`INSERT INTO Wallet (address) VALUES ('${wallet.from_wallet_address}') ON CONFLICT (address) DO UPDATE SET address = Wallet.address RETURNING id;`);
-            const to_wallet_id = await insert(`INSERT INTO Wallet (address) VALUES ('${wallet.to_wallet_address}') ON CONFLICT (address) DO UPDATE SET address = Wallet.address RETURNING id;`);
+                const from_wallet_id = await insert(`INSERT INTO Wallet (address) VALUES ('${wallet.from_wallet_address}') ON CONFLICT (address) DO UPDATE SET address = Wallet.address RETURNING id;`);
+                const to_wallet_id = await insert(`INSERT INTO Wallet (address) VALUES ('${wallet.to_wallet_address}') ON CONFLICT (address) DO UPDATE SET address = Wallet.address RETURNING id;`);
+    
+                const block_id = await insert(`INSERT INTO Block (hash, block_time) VALUES ('${block.hash}', ${block.block_time}) RETURNING id;`);
+    
+                let collection_id = await select(`SELECT id FROM Collection WHERE name = '${collection.name}' AND family = '${collection.family}' AND external_url = '${collection.external_url}';`);
+                if (!(collection_id[0] && collection_id[0].id)) {
+                    collection_id = await insert(`INSERT INTO Collection (name, family, external_url) VALUES ('${collection.name}','${collection.family}','${collection.external_url}') RETURNING id;`);
+                } else {
+                    collection_id = collection_id[0].id;
+                }
+                
+                const token_id = await insert(`INSERT INTO Token (collection_id, address, uri, asset_metadata, image_url, name, symbol, description, traits) VALUES (${collection_id},'${token.address}','${token.uri}','${token.asset_metadata}','${token.image_url}','${token.name}','${token.symbol}','${token.description}','${token.traits}') ON CONFLICT (address) DO UPDATE SET name = Token.name RETURNING id;`);
+    
+                const transaction_id = await insert(`INSERT INTO Transaction (signature, block_id, from_wallet_id, to_wallet_id, recent_blockHash, fee, value, vol) VALUES ('${transaction.signature}',${block_id},${from_wallet_id},${to_wallet_id},'${transaction.recent_blockhash}',${transaction.fee},${transaction.value},${transaction.vol}) RETURNING id;`);
+    
+                const transfer_id = await insert(`INSERT INTO Transfer (block_id, log, transaction_id, token_id, from_wallet_id, to_wallet_id) VALUES (${block_id},'${transfer.log}',${transaction_id},${token_id},${from_wallet_id},${to_wallet_id}) RETURNING id;`);
+                
+                await update(`UPDATE Token SET latest_transfer_id = ${transfer_id} WHERE id = ${token_id};`);
+         
+                //console.log(wallet);
+                //console.log(block);
+                //console.log(collection);
+                //console.log(token);
+                //console.log(transaction);
+                //console.log(transfer);    
+            }).catch(e => console.error(e));
 
-            const block_id = await insert(`INSERT INTO Block (hash, block_time) VALUES ('${block.hash}', ${block.blockTime}) RETURNING id;`);
-
-            let collection_id = await select(`SELECT id FROM Collection WHERE name = '${collection.name}' AND family = '${collection.family}' AND external_url = '${collection.external_url}';`);
-            if (!(collection_id[0] && collection_id[0].id)) {
-                collection_id = await insert(`INSERT INTO Collection (name, family, external_url) VALUES ('${collection.name}','${collection.family}','${collection.external_url}') RETURNING id;`);
-            } else {
-                collection_id = collection_id[0].id;
+            if (!transaction) {
+                console.error(`get failed for transaction: ${logs.signature}`);
             }
-            
-            const token_id = await insert(`INSERT INTO Token (collection_id, address, uri, asset_metadata, image_url, name, symbol, description, traits) VALUES (${collection_id},'${token.address}','${token.uri}','${token.asset_metadata}','${token.image_url}','${token.name}','${token.symbol}','${token.description}','${token.traits}') ON CONFLICT (address) DO UPDATE SET name = Token.name RETURNING id;`);
-
-            const transaction_id = await insert(`INSERT INTO Transaction (signature, block_id, from_wallet_id, to_wallet_id, recent_blockHash, fee, value, vol) VALUES ('${transaction.signature}',${block_id},${from_wallet_id},${to_wallet_id},'${transaction.recent_blockhash}',${transaction.fee},${transaction.value},${transaction.vol}) RETURNING id;`);
-
-            const transfer_id = await insert(`INSERT INTO Transfer (block_id, log, transaction_id, token_id, from_wallet_id, to_wallet_id) VALUES (${block_id},'${transfer.log}',${transaction_id},${token_id},${from_wallet_id},${to_wallet_id}) RETURNING id;`);
-            
-            await update(`UPDATE Token SET latest_transfer_id = ${transfer_id} WHERE id = ${token_id};`);
-     
-            //console.log(wallet);
-            //console.log(block);
-            //console.log(collection);
-            //console.log(token);
-            //console.log(transaction);
-            //console.log(transfer);
         }
     })
 })();
